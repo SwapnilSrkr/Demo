81ab3986676c502b992ea6558f36e305ef870f2c
Swapnil <swapnilsrkr@gmail.com>
2021-04-08 17:22:21 +0530

    Updated

--- Action.c
+++ Action.c
@@ -4,5 +4,11 @@
 
 Action()
 {
+	truclient_step("1", "Navigate to 'http://www.google.com/'", "snapshot=Action_1.inf");
+	truclient_step("3", "Click on Search combobox", "snapshot=Action_3.inf");
+	truclient_step("4", "Type GIT in Search combobox", "snapshot=Action_4.inf");
+	truclient_step("7", "Click on Search combobox", "snapshot=Action_7.inf");
+	truclient_step("8", "Press key Enter on Search combobox", "snapshot=Action_8.inf");
+
 	return 0;
 }
